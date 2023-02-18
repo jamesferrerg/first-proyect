b057b60fe0d093c5b8182d0d9fe661efd8a8857c
jamesferrerg <46761770+jamesferrerg@users.noreply.github.com>
2023-02-18 14:17:17 -0500

    Uso de async await

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,11 +1,21 @@
 const API = "https://api.themoviedb.org/3";
 
-export function get(path) {
-  return fetch(API + path, {
+export async function get(path) {
+
+  const response = await fetch(API + path, {
     headers: {
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNGQ0YzViYjA3OTIxNzExYjg3OTM3ZTU5MGU1OWZkYyIsInN1YiI6IjYzZWFkM2Q0ZjkyNTMyMDBjM2ZlZWMzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.piY_5j53vjYMjzUv9vNGnKxGwccvKEjTcvzVAzWFL6Y",
       "Content-Type": "application/json;charset=utf-8",
     },
-  }).then((result) => result.json());
+  });
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const items = await response.json();
+
+  return items;
 }
