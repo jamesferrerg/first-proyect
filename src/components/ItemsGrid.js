7c596134963526229e99ae1c42b5caae4976aee8
jamesferrerg <46761770+jamesferrerg@users.noreply.github.com>
2023-02-26 14:24:23 -0500

    Infinite scroll

--- src/components/ItemsGrid.js
+++ src/components/ItemsGrid.js
@@ -4,41 +4,48 @@ import { ItemCard } from "./ItemCard";
 import styles from "./ItemsGrid.module.css";
 import { Spinner } from "./Spinner";
 import { useSearchParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export function ItemsGrid() {
+export function ItemsGrid({ search }) {
   //let items = [];
   // para evitar un ciclo infinito definir un estado y se resuelve
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
-  const [query] = useSearchParams();
-  const search = query.get("search");
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
   // para que se ejecute nuevamente debe pasarse un parametro de lo contrario se ejecuta una sola vez
   useEffect(() => {
     setIsLoading(true);
     const searchUrl = search
-      ? "/search/movie?query=" + search
-      : "/discover/movie";
+      ? "/search/movie?query=" + search + "&page=" + page
+      : "/discover/movie?page=" + page;
     // promesa
     get(searchUrl).then((data) => {
       // results es un resultado de la respuesta
-      setItems(data.results);
+      setItems(prevItems => prevItems.concat(data.results));
+      setHasMore(data.page < data.total_pages);
       setIsLoading(false);
     });
     // la siguiente linea con el arreglo se evita los ciclos infinitos
     // el arreglo vacio me define que el efecto solo se ejecutara una sola vez
-  }, [search]);
-
-  if (isLoading) {
-    return <Spinner />;
-  }
+  }, [search, page]);
 
   return (
-    <ul className={styles.itemsGrid}>
-      {items.map((movie) => (
-        <ItemCard key={movie.id} item={movie} />
-      ))}
-    </ul>
+    // en la funcion next para ir pasando pagina a la siguiente puede realizarse con page + 1 
+    // pero es recomendable usar la funcion como se muestra en next, esto es por que se hace
+    // de forma asincrona
+    <InfiniteScroll
+      dataLength={items.length}
+      hasMore={hasMore}
+      next={() => setPage((prevPage => prevPage + 1))}
+      loader={<Spinner />}
+    >
+      <ul className={styles.itemsGrid}>
+        {items.map((movie) => (
+          <ItemCard key={movie.id} item={movie} />
+        ))}
+      </ul>
+    </InfiniteScroll>
   );
 }
